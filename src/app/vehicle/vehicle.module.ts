39dc400c3934ec4ca5214ab81dd6d16cb8a55b76
Carlos_Javier_Pita_Amaya <c.pita@uniandes.edu.co>
2022-04-30 15:30:06 -0500

    Creación módulo para vehiculos, y clase Vehiculo

---
+++ src/app/vehicle/vehicle.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { VehicleComponent } from './vehicle.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [VehicleComponent]
+})
+export class VehicleModule { }
